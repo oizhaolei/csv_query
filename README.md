400651ca8092180089baa3bfd78f708037fb5d0b
lei <oizhaolei@gmail.com>
2024-08-28 15:35:53 +0900

    match any in file

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
-Usage:
+## Usage:
 
-# csv_query --query "select * from ./xxxx.csv where res_code in (200, 201) and cs_byte>=1000 and url in-file-regex ./URLS.lst"
-cat original.csv | csv_query --query "res_code in (200, 201)"
-            | csv_query --query "cs_byte >= 1000"
-            | csv_query --query "url in-file-with-regex ./URLS.lst"
+``` shell
+-- csv_query "select * from ./xxxx.csv where res_code in (200, 201) and cs_byte>=1000 and url in-file-regex ./URLS.lst"
+cat original.csv | csv_query "res_code in (200, 201)"
+            | csv_query "cs_byte >= 1000"
+            | csv_query "url in-file-with-regex ./URLS.lst"
             | tee filtered.csv
+
+```
+
+## TODO
+- [ ] parse query string with `nom`
+- [ ] numeric field compare
+- [ ] in-file-with-regex
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,31 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 use std::{env, error::Error, io, process};
+
+fn read_file_to_vec(filename: &str) -> Result<Vec<String>, Box<dyn Error>> {
+    // Open the file in read-only mode (returns io::Result<File>)
+    let file = File::open(filename)?;
+
+    // Create a BufReader to efficiently read the file line by line
+    let reader = BufReader::new(file);
+
+    // Read lines into a Vec<String>
+    let lines: Vec<String> = reader
+        .lines() // returns an iterator over io::Result<String>
+        .collect::<Result<_, _>>()?; // collect into Vec<String>, handling errors
+
+    Ok(lines)
+}
 
 fn run() -> Result<(), Box<dyn Error>> {
     // Get the query from the positional arguments.
     // If one doesn't exist, return an error.
     let query = match env::args().nth(1) {
-        None => return Err(From::from("expected 1 argument, but got none")),
+        None => return Err(From::from("expected query-string, but got none")),
         Some(query) => query,
     };
+
+    //TODO parse query
 
     // Build CSV readers and writers to stdin and stdout, respectively.
     let mut rdr = csv::Reader::from_reader(io::stdin());
@@ -36,5 +55,52 @@ fn main() {
     if let Err(err) = run() {
         eprintln!("{}", err);
         process::exit(1);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use regex::Regex;
+
+    use crate::read_file_to_vec;
+    #[test]
+    fn test_is_match() {
+        let re = Regex::new(r"\b\w{13}\b").unwrap();
+        let hay = "I categorically deny having triskaidekaphobia.";
+        assert!(re.is_match(hay));
+    }
+    #[test]
+    fn test_is_match_any() {
+        let regex_list = vec!["^https://www.youtube.com", "^github.com"];
+        let re_list = regex_list
+            .iter()
+            .map(|r| {
+                let re = Regex::new(r).unwrap();
+                re
+            })
+            .collect::<Vec<Regex>>();
+
+        let hay = "https://www.youtube.com/watch?v=TLpufG9s0QY";
+
+        let match_any = re_list.iter().any(|re| re.is_match(hay));
+
+        assert!(match_any);
+    }
+    #[test]
+    fn test_is_match_regex_file() {
+        let re_list = read_file_to_vec("./url.regex.txt")
+            .unwrap()
+            .iter()
+            .map(|r| {
+                let re = Regex::new(r).unwrap();
+                re
+            })
+            .collect::<Vec<Regex>>();
+
+        let hay = "https://www.youtube.com/watch?v=TLpufG9s0QY";
+
+        let match_any = re_list.iter().any(|re| re.is_match(hay));
+
+        assert!(match_any);
     }
 }
