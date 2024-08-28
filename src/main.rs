9fa947f267e1e11f346b66c89d0f84413a40a364
lei <oizhaolei@gmail.com>
2024-08-28 21:47:22 +0900

    `nom` parse query string to enum

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::fmt::{self, Display};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::{env, error::Error, io, process};
@@ -13,89 +12,73 @@ use nom::IResult;
 
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
 pub enum Operator {
-    Not,
-    And,
-    Or,
-    Like,
-    NotLike,
-    Equal,
-    NotEqual,
-    Greater,
-    GreaterOrEqual,
-    Less,
-    LessOrEqual,
-    In,
-    NotIn,
-    Is,
-    Match,
-    MatchByFile,
+    Not(String),
+    And(String),
+    Or(String),
+    Like(String),
+    NotLike(String),
+    Equal(String),
+    NotEqual(String),
+    Greater(String),
+    GreaterOrEqual(String),
+    Less(String),
+    LessOrEqual(String),
+    In(String),
+    NotIn(String),
+    Is(String),
+    Match(String),
+    MatchByFile(String),
 }
 
-impl Display for Operator {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let op = match *self {
-            Operator::Not => "NOT",
-            Operator::And => "AND",
-            Operator::Or => "OR",
-            Operator::Like => "LIKE",
-            Operator::NotLike => "NOT_LIKE",
-            Operator::Equal => "=",
-            Operator::NotEqual => "!=",
-            Operator::Greater => ">",
-            Operator::GreaterOrEqual => ">=",
-            Operator::Less => "<",
-            Operator::LessOrEqual => "<=",
-            Operator::In => "IN",
-            Operator::NotIn => "NOT_IN",
-            Operator::Is => "IS",
-            Operator::Match => "MATCH",
-            Operator::MatchByFile => "MATCH_BY_FILE",
-        };
-        write!(f, "{}", op)
-    }
-}
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Condition {
     pub field: String,
     pub operate: Operator,
-    pub value: String,
 }
 
 fn till_space(s: &str) -> IResult<&str, &str> {
     take_till(|c| c == ' ')(s)
 }
-fn binary_comparison_operator(i: &str) -> IResult<&str, Operator> {
+
+fn binary_comparison_operator(i: &str, value: String) -> IResult<&str, Operator> {
     alt((
-        map(tag_no_case("not_like"), |_| Operator::NotLike),
-        map(tag_no_case("like"), |_| Operator::Like),
-        map(tag_no_case("!="), |_| Operator::NotEqual),
-        map(tag_no_case("<>"), |_| Operator::NotEqual),
-        map(tag_no_case(">="), |_| Operator::GreaterOrEqual),
-        map(tag_no_case("<="), |_| Operator::LessOrEqual),
-        map(tag_no_case("="), |_| Operator::Equal),
-        map(tag_no_case("<"), |_| Operator::Less),
-        map(tag_no_case(">"), |_| Operator::Greater),
-        map(tag_no_case("in"), |_| Operator::In),
-        map(tag_no_case("not_in"), |_| Operator::NotIn),
-        map(tag_no_case("match"), |_| Operator::Match),
-        map(tag_no_case("match_by_file"), |_| Operator::MatchByFile),
+        map(tag_no_case("not_like"), |_| {
+            Operator::NotLike(value.clone())
+        }),
+        map(tag_no_case("like"), |_| Operator::Like(value.clone())),
+        map(tag_no_case("!="), |_| Operator::NotEqual(value.clone())),
+        map(tag_no_case("<>"), |_| Operator::NotEqual(value.clone())),
+        map(tag_no_case(">="), |_| {
+            Operator::GreaterOrEqual(value.clone())
+        }),
+        map(tag_no_case("<="), |_| Operator::LessOrEqual(value.clone())),
+        map(tag_no_case("="), |_| Operator::Equal(value.clone())),
+        map(tag_no_case("<"), |_| Operator::Less(value.clone())),
+        map(tag_no_case(">"), |_| Operator::Greater(value.clone())),
+        map(tag_no_case("in"), |_| Operator::In(value.clone())),
+        map(tag_no_case("not_in"), |_| Operator::NotIn(value.clone())),
+        map(tag_no_case("match"), |_| Operator::Match(value.clone())),
+        map(tag_no_case("match_by_file"), |_| {
+            Operator::MatchByFile(value.clone())
+        }),
     ))(i)
 }
 
 fn parse_query(i: &str) -> IResult<&str, Condition> {
-    let (remain, (_, field, _, operate)) = tuple((
+    let (remain, (_, field, _, op, _)) = tuple((
         multispace0,
         till_space,
         multispace1,
-        binary_comparison_operator,
+        till_space,
+        multispace1,
     ))(i)?;
-
+    let value = remain.trim().to_string();
+    let (_, operate) = binary_comparison_operator(op, value.clone())?;
     Ok((
         "",
         Condition {
             field: field.to_string(),
             operate,
-            value: remain.trim().to_string(),
         },
     ))
 }
@@ -125,6 +108,28 @@ fn run() -> Result<(), Box<dyn Error>> {
     println!("query: {}", query);
 
     //TODO parse query
+    let (_, condition) = parse_query(&query).unwrap();
+    println!("condition: {:?}", condition);
+    match condition.operate {
+        Operator::Not(_) => {}
+        Operator::And(_) => {}
+        Operator::Or(_) => {}
+        Operator::Like(_) => {}
+        Operator::NotLike(_) => {}
+        Operator::Equal(_) => {}
+        Operator::NotEqual(_) => {}
+        Operator::Greater(_) => {}
+        Operator::GreaterOrEqual(_) => {}
+        Operator::Less(_) => {}
+        Operator::LessOrEqual(_) => {}
+        Operator::In(value) => {
+            println!("TODO: convert to vec:{}", value);
+        }
+        Operator::NotIn(_) => {}
+        Operator::Is(_) => {}
+        Operator::Match(_) => {}
+        Operator::MatchByFile(_) => {}
+    }
 
     // Build CSV readers and writers to stdin and stdout, respectively.
     let mut rdr = csv::Reader::from_reader(io::stdin());
@@ -198,7 +203,7 @@ mod tests {
     }
 
     #[test]
-    fn test_parse_query() {
+    fn test_parse_query_in() {
         let str = "res_code in (\"200\", \"201\")";
 
         assert_eq!(
@@ -207,8 +212,19 @@ mod tests {
                 "",
                 Condition {
                     field: "res_code".to_string(),
-                    operate: crate::Operator::In,
-                    value: "(\"200\", \"201\")".to_string(),
+                    operate: crate::Operator::In("(\"200\", \"201\")".to_string()),
+                }
+            ))
+        );
+        let str = "res_code IN (\"200\", \"201\")";
+
+        assert_eq!(
+            parse_query(str),
+            Ok((
+                "",
+                Condition {
+                    field: "res_code".to_string(),
+                    operate: crate::Operator::In("(\"200\", \"201\")".to_string()),
                 }
             ))
         );
